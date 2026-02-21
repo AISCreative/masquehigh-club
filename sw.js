fbe4fdbb39de9786cd02cf5dcab6394b4a777945
aisc.creative <lopjorn@gmail.com>
2026-02-21 21:36:35 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,19 @@
-const CACHE_NAME = 'masquehigh-v17';
+const CACHE_NAME = 'masquehigh-vFinal';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './images/image1.jpg',
-  './images/image2.jpg',
-  './images/image3.jpg',
-  './images/image4.jpg',
-  './images/image5.jpg',
-  './images/image6.jpg',
-  './images/image7.jpg',
-  './images/image8.jpg',
-  './images/image9.jpg',
-  './images/image10.jpg',
-  './images/image11.jpg',
-  './images/image12.jpg',
-  './images/image13.jpg',
-  './images/image14.jpg',
-  './images/image15.jpg',
-  './images/image16.jpg',
+  './images/image1.jpg', './images/image2.jpg', './images/image3.jpg', './images/image4.jpg',
+  './images/image5.jpg', './images/image6.jpg', './images/image7.jpg', './images/image8.jpg',
+  './images/image9.jpg', './images/image10.jpg', './images/image11.jpg', './images/image12.jpg',
+  './images/image13.jpg', './images/image14.jpg', './images/image15.jpg', './images/image16.jpg',
   './images/image17.jpg'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
