24b29ef3bad914103ae012a426c4434cd8f06b7d
aisc.creative <lopjorn@gmail.com>
2026-02-26 12:12:00 +0100

    Update service worker to include cache comments
    Added comment to clarify that certain images should not be included in the cache.

--- sw.js
+++ sw.js
@@ -8,6 +8,7 @@ const ASSETS = [
   './images/image9.jpg', './images/image10.jpg', './images/image11.jpg', './images/image12.jpg',
   './images/image13.jpg', './images/image14.jpg', './images/image15.jpg', './images/image16.jpg',
   './images/image17.jpg'
+  // NO incluyas gorila.jpg ni placeholder.jpg porque se añadirán desde la tablet
 ];
 
 self.addEventListener('install', (e) => {
