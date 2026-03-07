eacf7a0cc4e387281ee1a354c873b07627df2eda
aisc.creative <lopjorn@gmail.com>
2026-03-07 11:46:20 +0100

    Update service worker cache name and assets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'masquehigh-vFinal';
+const CACHE_NAME = 'masquehigh-v3';
 const ASSETS = [
   './',
   './index.html',
@@ -7,14 +7,34 @@ const ASSETS = [
   './images/image5.jpg', './images/image6.jpg', './images/image7.jpg', './images/image8.jpg',
   './images/image9.jpg', './images/image10.jpg', './images/image11.jpg', './images/image12.jpg',
   './images/image13.jpg', './images/image14.jpg', './images/image15.jpg', './images/image16.jpg',
-  './images/image17.jpg'
-  // NO incluyas gorila.jpg ni placeholder.jpg porque se añadirán desde la tablet
+  './images/image17.jpg', './images/gorila.jpg', './images/placeholder.jpg'
 ];
 
+// Instalación
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
 
+// Activación
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Interceptar peticiones
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => res || fetch(e.request))
+      .catch(() => caches.match('./index.html'))
+  );
 });
